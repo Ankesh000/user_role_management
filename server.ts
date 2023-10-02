61223533e0eb520073dba966f7140d210f533ade
Ankesh000 <ankesh@glocify.com>
2023-10-02 14:26:20 +0530

    Update server.ts
    updated

--- server.ts
+++ server.ts
@@ -1,29 +1,44 @@
-import express from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import sequelize from './config/database';
-import indexRoutes from './src/routes/indexRoute'
-const app = express();
-import  dotenv from 'dotenv'
-dotenv.config()
+import indexRoutes from './src/routes/indexRoute';
+import dotenv from 'dotenv';
 
-// Set up middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/api/v1', indexRoutes);
-app.use(bodyParser.urlencoded({ extended: true }))
-// Start the Express server
-const port = process.env.PORT || 4000;
-sequelize.sync()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Error syncing the database:', err);
-  });
-  //updated commit
-  // $ echo $PATH
-  // $PATH="/c/Program Files/PostgreSQL/16/bin:$PATH"
-  // $pg_ctl.exe start -D "C:\Program Files\PostgreSQL\16\data"
+class Server {
+  private app: Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+    this.start();
+  }
+
+  private config(): void {
+    dotenv.config();
+    this.app.use(bodyParser.json());
+    this.app.use(cors());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
+
+  private routes(): void {
+    this.app.use('/api/v1', indexRoutes);
+  }
+
+  private start(): void {
+    const port = process.env.PORT || 4000;
+    sequelize
+      .sync()
+      .then(() => {
+        this.app.listen(port, () => {
+          console.log(`Server is running on port ${port}`);
+        });
+      })
+      .catch((err) => {
+        console.error('Error syncing the database:', err);
+      });
+  }
+}
+
+new Server();
